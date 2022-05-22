bd2f0bd6dd7709f84b0efd34d307b33b4bcb0362
petrusel <104720241+petrusel@users.noreply.github.com>
2022-05-22 13:04:53 +0300

    Update dictionarScript.js

--- dictionarScript.js
+++ dictionarScript.js
@@ -1,31 +1,31 @@
 const dictionar = [];
-		let indexWord = 0;
+let indexWord = 0;
 
-		// INSERT
-		document.getElementById("insertWord_input").addEventListener("keypress", function(event) {
-		  if (event.key === "Enter") {
-		    addWord();
-		  }
-		});
+// INSERT
+document.getElementById("insertWord_input").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    addWord();
+  }
+});
 
-		function addWord() {
-			dictionar[indexWord] = document.getElementById("insertWord_input").value;
-			document.getElementById("status").innerHTML = "Cuvantul '" + dictionar[indexWord] + "' a fost inserat.";
-			++indexWord;
-		}
+function addWord() {
+	dictionar[indexWord] = document.getElementById("insertWord_input").value;
+	document.getElementById("status").innerHTML = "Cuvantul '" + dictionar[indexWord] + "' a fost inserat.";
+	++indexWord;
+}
 
-		// SEARCH
-		document.getElementById("searchWord_input").addEventListener("keypress", function(event) {
-		  if (event.key === "Enter") {
-		    searchWord();
-		  }
-		});
+// SEARCH
+document.getElementById("searchWord_input").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    searchWord();
+  }
+});
 
-		function searchWord() {
-			const word = document.getElementById("searchWord_input").value;
-			if (dictionar.includes(word)) {
-				document.getElementById("status").innerHTML = "Cuvantul '" + word + "' a fost gasit in lista.";
-			} else {
-				document.getElementById("status").innerHTML = "Cuvantul '" + word + "' nu exista in lista.";
-			}
-		}
+function searchWord() {
+	const word = document.getElementById("searchWord_input").value;
+	if (dictionar.includes(word)) {
+		document.getElementById("status").innerHTML = "Cuvantul '" + word + "' a fost gasit in lista.";
+	} else {
+		document.getElementById("status").innerHTML = "Cuvantul '" + word + "' nu exista in lista.";
+	}
+}
